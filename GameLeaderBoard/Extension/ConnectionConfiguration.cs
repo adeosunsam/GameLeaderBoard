4c3c9e559879cc34ae08de00b3b07560f2bc3c9b
adeosunsam <adeosunsamuel30@gmail.com>
2024-03-15 10:03:44 +0100

    Update ConnectionConfiguration.cs

--- GameLeaderBoard/Extension/ConnectionConfiguration.cs
+++ GameLeaderBoard/Extension/ConnectionConfiguration.cs
@@ -14,7 +14,7 @@ namespace GameLeaderBoard.Extension
             string db = databaseUri.LocalPath.TrimStart('/');
             string[] userInfo = databaseUri.UserInfo.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};" +
+            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port=5432;" +
             $"Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
 
         }*/
